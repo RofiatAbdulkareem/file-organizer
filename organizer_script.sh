d01ae2ad81fde5e26a88f4bab631ff5bff382bd8
Rofiat <oyizakareem9@gmail.com>
2024-10-28 15:13:15 +0100

    first commit

---
+++ organizer_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Move .txt files to Text_Files folder
+mv *.txt Text_Files/ 2>/dev/null
+
+# Move .jpg files to Images folder
+mv *.jpg Images/ 2>/dev/null
+
+# Move .zip files to Archives folder
+mv *.zip Archives/ 2>/dev/null
